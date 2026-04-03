e9b9e208605c3724ed45c6ba188bd9ce950a2aac
Panosigalas <147259556+Panosigalas@users.noreply.github.com>
2026-04-03 12:58:57 +0300

    Add constructor + consume()

--- StringEvaluator.java
+++ StringEvaluator.java
@@ -1,7 +1,17 @@
 public class StringEvaluator {
   private int lookahead;
   private InputStream input;
+  
   public StringEvaluator(InputStream input) {
       this.input = input;
+      consume();
   }
+  
+  private void consume() {
+      try {
+          lookahead = input.read();
+      } catch (IOException e) {
+          lookahead = -1;
+      }
+    }
 }

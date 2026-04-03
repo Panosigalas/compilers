931af320b995869d5ef072870f864b3c341ce0eb
Panosigalas <147259556+Panosigalas@users.noreply.github.com>
2026-04-03 13:21:54 +0300

    Update StringEvaluator.java

--- StringEvaluator.java
+++ StringEvaluator.java
@@ -1,17 +1,27 @@
 public class StringEvaluator {
   private int lookahead;
   private InputStream input;
-  
+
   public StringEvaluator(InputStream input) {
-      this.input = input;
-      consume();
+    this.input = input;
+    consume();
   }
-  
+
+  // read next char
   private void consume() {
-      try {
-          lookahead = input.read();
-      } catch (IOException e) {
-          lookahead = -1;
-      }
+    try {
+      lookahead = input.read();
+    } catch (IOException e) {
+      lookahead = -1;
     }
+  }
+
+  // make sure that i found what i expected
+  private void match(int expected) throws Exception {
+    if (lookahead == expected) {
+      consume();
+    } else {
+      throw new Exception("Parse Error");
+    }
+  }
 }
